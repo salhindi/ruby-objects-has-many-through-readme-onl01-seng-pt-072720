285508b9c1c050ec6f7c1f7f27c2201948fb919a
Sara A <calisaraalhindi@gmail.com>
2020-09-03 02:12:10 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,11 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter= waiter
+    @customer= customer
+    @total= total
+    @tip= tip
+  end
 
 end
