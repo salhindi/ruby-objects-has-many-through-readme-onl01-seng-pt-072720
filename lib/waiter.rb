38985baf38c7790fd7f761225f94b0e6aa1ee6ea
Sara A <calisaraalhindi@gmail.com>
2020-09-03 02:10:19 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,16 @@
 class Waiter
   attr_accessor :name, :experience
   
+  @@all = []
+  
   def initialize(name, experience)
     @name= name
     @experience= experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 
 end
